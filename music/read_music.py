46a1f0c09373474522d3cfb9d964dc56e82df557
Phill Conrad <pconrad@cs.ucsb.edu>
2017-08-16 11:33:26 -0700

    music

---
+++ music/read_music.py
@@ -0,0 +1,20 @@
+
+import json
+from pprint import pprint
+
+with open('music.json') as json_data:
+    music = json.load(json_data)
+
+print("type(music)=",type(music))
+print("len(music)=",len(music))
+
+##print("music[0]=",end="")
+##
+##print(music[0])
+##
+##pprint(music[0])
+##
+##for m in music:
+##    print ('{:30} {:30}'.format(m["artist"]["name"],m["song"]["title"]))
+
+
